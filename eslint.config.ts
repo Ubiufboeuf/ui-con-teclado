49cec2a37473f9733c9a3950cc8b118c4b0a8cb5
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2025-10-17 20:04:46 -0300

    chore: instalar y configurar eslint

---
+++ eslint.config.ts
@@ -0,0 +1,28 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import css from '@eslint/css'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts,mts,cts}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
+  {
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      'no-empty-pattern': 'off',
+      '@stylistic/quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      '@stylistic/eol-last': ['error', 'always'],
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'space-before-function-paren': ['error', 'always'],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      'no-unused-vars': ['warn', 'always']
+    }
+  }
+])
